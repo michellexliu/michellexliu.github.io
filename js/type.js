593b75b2bdf10dc53d0134b1a54f6ebf5634e521
Michelle <mliuu3259@gmail.com>
2020-08-27 15:03:28 -0400

    capitalization

--- js/type.js
+++ js/type.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(event){
   // array with texts to type in typewriter
-  var dataText = [ "a student.", "a developer.", "a designer.", "michelle."];
+  var dataText = [ "a student.", "a developer.", "a designer.", "Michelle."];
   
   // type one text in the typwriter
   // keeps calling itself until the text is finished
