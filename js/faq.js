c180f3016cfc23d561b6b22bc6e29116961260ec
Michelle <mliuu3259@gmail.com>
2020-08-27 14:35:19 -0400

    changed panels

--- js/faq.js
+++ js/faq.js
@@ -1,3 +1,20 @@
+const accordions = document.getElementsByClassName("accordion");
+for (var i = 0; i < accordions.length; i++) {
+	accordions[i].addEventListener("click", function() {
+		/* Toggle between adding and removing the "active" class,
+		to highlight the button that controls the panel */
+		this.classList.toggle("active");
+
+		/* Toggle between hiding and showing the active panel */
+		var panel = this.nextElementSibling;
+		if (panel.style.maxHeight) {
+			panel.style.maxHeight = null;
+		} else {
+			panel.style.maxHeight = panel.scrollHeight + "px";
+		}
+	});
+}
+/*
 var acc = document.getElementsByClassName("accordion"); //Array with all accordion-style opening questions
 var i;
 
@@ -11,4 +28,4 @@ for (i = 0; i < acc.length; i++) {  //Loops through every question to add event 
       panel.style.maxHeight = panel.scrollHeight + "px";
     }
   });
-}
+}*/
